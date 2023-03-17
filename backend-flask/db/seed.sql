18c629cef6f7b9aa14389600e08004a3467be389
roopish <rmurthy3554@gmail.com>
2023-03-17 23:05:24 +0000

    env updates, table column update

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,10 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'andrewbrown' ,'a@br.com', 'MOCK'),
+  ('Andrew Bayko', 'bayko' ,'a@ba.com','MOCK');
 
+\echo 'inserted into users table'
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
@@ -11,3 +12,4 @@ VALUES
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
+\echo 'inserted into activities table'
