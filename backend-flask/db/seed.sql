a49d357b306e8a46c036466a2ad679696d3300c0
roopish <rmurthy3554@gmail.com>
2023-03-31 05:39:44 +0000

    Add new row to seed;  comment out unnecessary code

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,7 +3,8 @@ INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
   ('roopish', 'roopish' ,'rm@gmail.com','26b882d5-8c11-4bea-a7e9-bb0a1ef65742'),
   ('Andrew Brown', 'andrewbrown' ,'a@br.com', 'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'a@ba.com','MOCK');
+  ('Andrew Bayko', 'bayko' ,'a@ba.com','MOCK'),
+  ('Londo Mollari','londo' ,'lmollari@centari.com' ,'MOCK');
 
 \echo 'inserted into users table'
 INSERT INTO public.activities (user_uuid, message, expires_at)
