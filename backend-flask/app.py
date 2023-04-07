a811220388814c2bc79dc00fae710c0f0a195361
roopish <rmurthy3554@gmail.com>
2023-04-07 00:00:10 +0000

    backend changes Fargate container, service,policy,roles

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -130,11 +130,17 @@ def init_rollbar():
     # send exceptions from `app` to rollbar, using flask's signal system.
     got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
-# rollbar---
-@app.route('/rollbar/test')
-def rollbar_test():
-    rollbar.report_message('Hello World!', 'warning')
-    return "Hello World!"
+#flask health check added week6
+@app.route('/api/health-check')
+def health_check():
+  return {'success': True}, 200
+
+
+# rollbar--- commented week6
+#@app.route('/rollbar/test')
+#def rollbar_test():
+#    rollbar.report_message('Hello World!', 'warning')
+#    return "Hello World!"
 
 # @app.route("/api/message_groups", methods=['GET'])
 # def data_message_groups():
