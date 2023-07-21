045c4565e06ceeb8c96c61742e3e874b088abade
roopish <rmurthy3554@gmail.com>
2023-07-21 05:17:38 +0000

    update function.rb

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -3,51 +3,49 @@ require 'json'
 require 'jwt'
 
 def handler(event:, context:)
-    
-    origin = event["headers"]["origin"]
-    puts("origin", origin)
-    myfrontentend_dev_url = "https:\/\/3000-datacleansi-awsbootcamp-(.+)\.gitpod\.io"
-    if !(/#{myfrontentend_dev_url}/.match(origin))  # if there is no match, then return the prod domain
-      origin = "api.roopish-awssolutions.com"
+    puts "event:"
+    puts event
+    produrl="https:\/\/3000-datacleansi-awsbootcamp-(.+)\.gitpod\.io"
+    if event['routeKey'] == "OPTIONS /{proxy+}"
+      puts({step: 'preflight', message: 'preflight CORS check'}.to_json)
+      { 
+        headers: {
+          "Access-Control-Allow-Headers": "*, Authorization",
+          "Access-Control-Allow-Origin": produrl,
+          "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+        },
+        statusCode: 200
+      }
+    else
+      token = event['headers']['authorization'].split(' ')[1]
+      puts({step: 'presignedurl', access_token: token}.to_json)
+  
+      body_hash = JSON.parse(event["body"])
+      extension = body_hash["extension"]
+      puts({ extension: extension }.to_json)
+      
+      decoded_token = JWT.decode token, nil, false
+      cognito_user_uuid = decoded_token[0]['sub']
+      puts({ cognito_user_uuid: cognito_user_uuid }.to_json)
+  
+      s3 = Aws::S3::Resource.new
+      bucket_name = ENV["UPLOADS_BUCKET_NAME"]
+      object_key = "#{cognito_user_uuid}.#{extension}"
+      puts({object_key: object_key}.to_json)
+  
+      obj = s3.bucket(bucket_name).object(object_key)
+      url = obj.presigned_url(:put, expires_in: 60 * 5)
+      url # this is the data that will be returned
+      puts({ presigned_url: url }.to_json)
+      body = {url: url}.to_json # this is the data that will be returned
+      { 
+        headers: {
+          "Access-Control-Allow-Headers": "*, Authorization",
+          "Access-Control-Allow-Origin": produrl,
+          "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
+        },
+        statusCode: 200, 
+        body: body 
+      }
     end
-    # return cors headers for preflight check
-  if event['routeKey'] == "OPTIONS /{proxy+}"
-    puts({step: 'preflight', message: 'preflight CORS check'}.to_json)
-    { 
-      headers: {
-        "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": origin,
-        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
-      },
-      statusCode: 200
-    }
-  else
-    token = event['headers']['authorization'].split(' ')[1]
-    puts({step: 'presignedurl', access_token: token}.to_json)
-
-    body_hash = JSON.parse(event["body"])
-    extension = body_hash["extension"]
-
-    decoded_token = JWT.decode token, nil, false
-    cognito_user_uuid = decoded_token[0]['sub']
-
-    s3 = Aws::S3::Resource.new
-    bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-    object_key = "#{cognito_user_uuid}.#{extension}"
-    puts({object_key: object_key}.to_json)
-
-    obj = s3.bucket(bucket_name).object(object_key)
-    url = obj.presigned_url(:put, expires_in: 60 * 5)
-    url # this is the data that will be returned
-    body = {url: url}.to_json # this is the data that will be returned
-    { 
-      headers: {
-        "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": origin,
-        "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
-      },
-      statusCode: 200, 
-      body: body 
-    }
-  end
 end
