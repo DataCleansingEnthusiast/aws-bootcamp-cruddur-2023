7737026754b266427848c01b4c7e3737d7eef3aa
roopish <rmurthy3554@gmail.com>
2023-07-22 05:34:03 +0000

    remove console log; rollbar env add;

--- frontend-react-js/src/components/FormErrors.js
+++ frontend-react-js/src/components/FormErrors.js
@@ -5,7 +5,7 @@ export default function FormErrors(props) {
   let el_errors = null
 
   if (props.errors.length > 0) {
-    console.log(props.errors)
+    
     el_errors = (<div className='errors'>
       {props.errors.map(err_code => {
         return <FormErrorItem err_code={err_code} />
--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -4,7 +4,7 @@ export default function ProfileAvatar(props) {
   const backgroundImage = `url("https://assets.roopish-awssolutions.com/avatars/${props.id}.jpg")`
    
   // console.log("ProfileAvatar", props)
-   console.log(backgroundImage)
+   //console.log(backgroundImage)
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
